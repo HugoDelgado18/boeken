d97d2ff7b82c2d7574990da7821ed8fa3be6421d
hugo delgado <hugo.delgado1@outlook.com>
2025-04-11 00:30:59 -0600

    user create

--- boeken/src/main/java/com/boeken/boeken/services/UserService.java
+++ boeken/src/main/java/com/boeken/boeken/services/UserService.java
@@ -1,4 +1,10 @@
 package com.boeken.boeken.services;
 
+import com.boeken.boeken.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
+
+    public User create(User user);
 }
--- boeken/src/main/java/com/boeken/boeken/services/UserServiceImpl.java
+++ boeken/src/main/java/com/boeken/boeken/services/UserServiceImpl.java
@@ -1,4 +1,21 @@
 package com.boeken.boeken.services;
 
+import com.boeken.boeken.models.User;
+import com.boeken.boeken.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
 }
