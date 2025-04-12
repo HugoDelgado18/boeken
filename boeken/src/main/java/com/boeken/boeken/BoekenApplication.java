7eb5ee49008a7ac62e033050f3b8055ad2e38662
hugo delgado <hugo.delgado1@outlook.com>
2025-04-11 20:49:24 -0600

    app

---
+++ boeken/src/main/java/com/boeken/boeken/BoekenApplication.java
@@ -0,0 +1,13 @@
+package com.boeken.boeken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoekenApplication.class, args);
+	}
+
+}
